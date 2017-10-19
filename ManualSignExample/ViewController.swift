e6b2c6a5990d221e60b086813b640fb8ddc27266
Stephen St. Martin <steve@stevestmartin.com>
2017-10-19 13:54:50 -0400

    Initial Commit

---
+++ ManualSignExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManualSignExample
+//
+//  Created by Stephen St. Martin on 10/19/17.
+//  Copyright © 2017 Steve St Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
